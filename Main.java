e9aa2827e175372a15444b7ef870f4782bf9b13c
jneeley3 <jneeley3@charlotte.edu>
2025-10-19 00:18:24 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new Frame());
-        //BodyPart testtest = new BodyPart();
-        //testtest.WriteSymptomsToFile();
-    }
-}
